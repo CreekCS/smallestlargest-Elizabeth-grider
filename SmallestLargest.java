7f6c5aa832c1d765f8c662253c108f6d4ea38c85
Elizabeth-grider <148920090+Elizabeth-grider@users.noreply.github.com>
2024-08-29 19:13:02 +0000

    COMMITING!

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,39 @@
 //put your name here
 public class SmallestLargest{
-    public static void main(String[] args){
+    public static int[] arg = new int[10];
+    public static void main(String args[]){
+         
+        for(int i =0; i<10; i++){
+            arg[i]= (int)(Math.random()*100)+1; 
+        }
+        
+        System.out.println(smallestValue()); 
+        System.out.println(largestValue()); 
+
 
     }
     public static int smallestValue(){
+        int small= arg[0]; 
 
+        for(int i =0; i<9; i++){
+        if (arg[i]<small){
+            small= arg[i]; 
+        }
+            
+        
+        }
+        return small; 
     }
     public static int largestValue(){
+        int big= arg[0]; 
+        for(int i =0; i<9; i++){
+        if (arg[i]>big){
+            big= arg[i]; 
+        }
+           
         
+        }
+         return big; 
     }
+
 }
